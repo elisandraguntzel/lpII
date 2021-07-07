3cff8c98f574d02745f54b6c34db1a6564623f99
elisandraguntzel <elisandraguntzel29@gmail.com>
2021-07-06 21:20:31 -0300

    atualizado 06/07

--- src/Atividade7/Ex2.java
+++ src/Atividade7/Ex2.java
@@ -1,4 +1,44 @@
 package Atividade7;
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Ex2 {
+
+    public static void main(String[] args) {
+        Scanner ler = new Scanner(System.in);
+        ArrayList<Integer> listanumero = new ArrayList<>();
+        ArrayList<Integer> excluipar = new ArrayList<>();
+
+        int n;
+
+
+        while (listanumero.size() < 10) {
+
+            System.out.println("Informe um numero ");
+            n = ler.nextInt();
+            listanumero.add(n);
+
+        }
+
+        System.out.println("Lista Original:");
+        for(Integer lista : listanumero) {
+            System.out.print(lista + " ");
+        }
+
+
+
+        for (Integer lista : listanumero) {
+            if (lista % 2 != 0) {
+                excluipar.add(lista);
+
+            }
+        }
+
+        System.out.println("\n\nLista sem os números pares: ");
+        for (Integer lista : excluipar) {
+            System.out.print(" " + lista);
+        }
+
+
+    }
 }
--- src/Atividade7/Ex3.java
+++ src/Atividade7/Ex3.java
@@ -1,4 +1,62 @@
 package Atividade7;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
+
 public class Ex3 {
+
+    public static void main(String[] args) {
+        Scanner ler = new Scanner(System.in);
+
+        ArrayList<String> lista = new ArrayList<>();
+
+        String s = " ";
+        int aux = 0;
+
+        while (true){
+
+            System.out.println("Escolha uma das opções: \n");
+            System.out.print("1 - cadastrar \n");
+            System.out.print("2 - listas \n");
+            System.out.print("3 - remover \n");
+            System.out.print("0 - sair \n");
+
+            aux = ler.nextInt();
+
+            switch (aux){
+                case 1:
+                    System.out.print("Informe o nome: ");
+                    ler.nextLine();
+                    s = ler.nextLine();
+                    System.out.print("Informe o número: ");
+                    String num = ler.nextLine();
+                    lista.add(s + " " + num);
+                    break;
+                case 2:
+                    Collections.sort(lista);
+                    System.out.println("Lista ordenada ");
+                    for(int i = 0; i < lista.size(); i++){
+                        System.out.printf("%d - %s\n", i, lista.get(i));
+                    }
+                    break;
+                case 3:
+                    System.out.print("Informe o índice  que deseja remover: ");
+                    int rem = ler.nextInt();
+                    lista.remove(rem);
+                    break;
+                default:
+
+            }
+
+            if(aux == 0){
+                System.out.println("Programa finalizado.");
+
+            }
+
+        }
+
+
+    }
 }
+
